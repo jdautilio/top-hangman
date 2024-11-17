f91e119a36ffe8b201e4ca3f76cab5d3fed3679b
Joel D'Autilio <joeldautilio@gmail.com>
2024-11-17 19:37:16 -0300

    Save game

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -4,16 +4,42 @@ require_relative 'board'
 require_relative 'game'
 include Board # rubocop:disable Style/MixinUsage
 
+def save(game, filename)
+  serialized_game = Marshal.dump(game)
+  Dir.mkdir('saves') unless Dir.exist? 'saves'
+
+  File.open(File.join('saves', filename), 'w') do |file|
+    file.write serialized_game
+  end
+end
+
+def load(filename)
+  Marshal.load File.read(File.join('saves', filename)) # rubocop:disable Security/MarshalLoad
+end
+
 dictionary = File.readlines('google-10000-english-no-swears.txt', chomp: true)
-dictionary.filter { |w| w.length >= 5 && w.length <= 12 }.sample
+word = dictionary.filter { |w| w.length >= 5 && w.length <= 12 }.sample
 
-game = Game.new('balas', 10)
+puts 'Load game? [y/N]'
+should_load = gets[0].downcase == 'y'
+
+game =
+  if should_load
+    load('game')
+  else
+    Game.new(word, 10)
+  end
+
 render(game)
-
 until game.ended?
   printf 'Your guess: '
   guess = $stdin.gets[0].downcase
   game.guess(guess)
 
   render(game)
+
+  puts 'Save? [Y,n]'
+  save = gets[0].downcase != 'n'
+
+  save(game, 'game') if save
 end
